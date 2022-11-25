c038e410f1b3399ea616787057054151d92a7ebd
TechMVinayaka <108673430+TechMVinayaka@users.noreply.github.com>
2022-11-25 13:00:11 +0530

    Update HelloWorld.java
    Git commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld{
 	
 	public static void main(String[] args){
 		System.out.println("**********Hello World & Welcome to Jenkins *********");
-		System.out.println("**********Made some changes *********");
 	}
 }
